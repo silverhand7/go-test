db5bd3c043a1c99be50a59ce7d21240b4aac34d3
silverhand7 <refojunior5@gmail.com>
2023-02-02 00:31:12 +0800

    learning test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,23 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMain(m *testing.M) {
+	// before
+	// This is usually used to do something before run the test, for example connect to DB, prepare the data, etc.
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run()
+
+	// after unit test
+	fmt.Println("AFTER UNIT TEST")
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Silverhand")
@@ -23,4 +38,26 @@ func TestHelloWorldGeralt(t *testing.T) {
 	}
 
 	fmt.Println("TestHelloWorldGeralt Done")
+}
+
+func TestHelloWorldAssertEqual(t *testing.T) {
+	result := HelloWorld("Jon")
+	assert.Equal(t, "Hello Jon", result, "They should be equal")
+	fmt.Println("Assert done") // it will be executed because assert above using Fail
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Snow")
+	require.Equal(t, "Hello Snow", result, "Result Must be Hello Snow")
+	fmt.Println("Will not be executed if it fails") // using FailNow()
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		// using t.Skip() to skip the test on certain condition
+		t.Skip("Can't run on MacOs")
+	}
+
+	result := HelloWorld("Jon")
+	assert.Equal(t, "Hello Jon", result, "They should be equal")
 }
