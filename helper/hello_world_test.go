be9d2fbc5769527714f70cf706c785fa183e45ef
silverhand7 <refojunior5@gmail.com>
2023-02-01 23:30:39 +0800

    thwart unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,10 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Silverhand")
 	if result != "Hello Silverhand" {
-		panic("Result is not 'Hello Silverhand'")
+		// t.Fail()
+		t.Error("Result must be 'Hello Silverhand'")
 	}
+
+	fmt.Println("TestHelloWorld Done")
+}
+
+func TestHelloWorldGeralt(t *testing.T) {
+	result := HelloWorld("Geralt")
+	if result != "Hello Geralt" {
+		// t.FailNow()
+		t.Fatal("Result must be 'Hello Geralt'")
+	}
+
+	fmt.Println("TestHelloWorldGeralt Done")
 }
